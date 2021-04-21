b4f738898965cd269d1319dcf6d80eaabd6cfa35
ThickPropheT <ThickPropheT@github.com>
2021-04-20 23:48:02 -0500

    Migrates accessory building logic to accessory_factory.

---
+++ accessory_factory.c
@@ -0,0 +1,34 @@
+#include "accessory_factory.h"
+
+#include "memory_pak.h"
+#include "rumble_pak.h"
+
+static struct accessory* resolve_rpak(u8 i_slot)
+{
+	// Brawler64 controllers report mempaks as a rumble pak.
+	// TODO could/should the result of this call be cached?
+	if (!validate_mempak(i_slot))
+		return (struct accessory*)mpk_new(i_slot);
+
+	return (struct accessory*)rpk_new(i_slot);
+}
+
+struct accessory* resolve_acc(struct device_state dev, u8 i_slot)
+{
+	u8 type = ACCESSORY_NONE;
+
+	if (dev.acc_flags & get_flag(i_slot))
+		type = identify_accessory(i_slot);
+
+	switch (type)
+	{
+	case ACCESSORY_MEMPAK:
+		return (struct accessory*)mpk_new(i_slot);
+
+	case ACCESSORY_RUMBLEPAK:
+		return resolve_rpak(i_slot);
+
+	default:
+		return acc_new(i_slot);
+	}
+}
---
+++ accessory_factory.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "device_state.h"
+
+struct accessory* resolve_acc(struct device_state dev, u8 i_slot);
