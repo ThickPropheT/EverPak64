a0a8e8d27967f005c07fd91903e329e1346a7ce9
ThickPropheT <ThickPropheT@github.com>
2021-04-21 00:11:22 -0500

    Migrates mem pak constants from accessory to memory_pak where the belong.

--- accessory.h
+++ accessory.h
@@ -8,11 +8,6 @@
 #define N_ACC_TYPES		5
 
 
-#define MPAK_STATUS_READY 			 0
-#define MPAK_STATUS_UNREADABLE		-2
-#define MPAK_STATUS_UNFORMATTED		-3
-
-
 struct accessory
 {
 	// TODO should this be const?
--- memory_pak.h
+++ memory_pak.h
@@ -4,6 +4,13 @@
 
 #define ENTRIES_LEN		16
 
+
+#define MPAK_STATUS_READY 			 0
+#define MPAK_STATUS_UNREADABLE		-2
+#define MPAK_STATUS_UNFORMATTED		-3
+
+
+
 struct memory_pak
 {
 	// TODO should this be const?
