1088bdd6769e9549b631f2db13968db8579a735e
ThickPropheT <ThickPropheT@github.com>
2021-04-27 00:40:19 -0500

    Cleans up mpk_menu_builder inheritance 'unpacking' logic.

--- mpk_menu_builder.c
+++ mpk_menu_builder.c
@@ -23,30 +23,29 @@ struct mpk_menu_builder* mpkmb_new(struct device_state* dev, struct menu_nav_con
 }
 
 
-static struct game_menu* build_menu_impl(struct mpk_menu_builder* mmb, struct acc_mb_args* args, struct memory_pak* mpk)
+static inline void* unpack(struct menu_builder* mb, struct mb_args* args, struct memory_pak** mpk)
 {
-	return (void*)mpkm_new(mmb->dev, mmb->mnav, mpk);
+	*mpk = (void*)((struct acc_mb_args*)args)->acc;
+	return mb;
 }
 
 static struct game_menu* build_menu(struct menu_builder* mb, struct mb_args* args)
 {
-	struct mpk_menu_builder* mmb = (void*)mb;
-	struct acc_mb_args* ama = (void*)args;
+	struct memory_pak* mpk = NULL;
+	struct mpk_menu_builder* mmb = unpack(mb, args, &mpk);
 
-	struct memory_pak* mpk = (void*)ama->acc;
-
-	return build_menu_impl(mmb, ama, mpk);
+	return (void*)mpkm_new(mmb->dev, mmb->mnav, mpk);
 }
 
 static struct menu_presenter* build_presenter(struct menu_builder* mb, struct mb_args* args)
 {
-	struct mpk_menu_builder* mmb = (void*)mb;
-	struct acc_mb_args* ama = (void*)args;
+	struct memory_pak* mpk = NULL;
+	struct mpk_menu_builder* mmb = unpack(mb, args, &mpk);
 
-	struct memory_pak* mpk = (void*)ama->acc;
+	struct device_state* dev = mmb->dev;
 
-	struct game_menu* gm = build_menu_impl(mmb, ama, mpk);
-	struct menu_presenter* mp = (void*)accmp_new(gm, mmb->mbt, mmb->dev, mpk->acc.i_slot);
+	struct game_menu* gm = (void*)mpkm_new(dev, mmb->mnav, mpk);
+	struct menu_presenter* mp = (void*)accmp_new(gm, mmb->mbt, dev, mpk->acc.i_slot);
 
 	return mp;
 }
