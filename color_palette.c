4d3813a4865217624f0a8262ce2c67153a5f8103
ThickPropheT <ThickPropheT@github.com>
2022-02-22 18:56:25 -0600

    Updates color palette to consist of a nice blue-green theme.

--- color_palette.c
+++ color_palette.c
@@ -10,11 +10,17 @@ struct color_palette *cp_new(void)
 	cp->trans = graphics_make_color(0xFF, 0xFF, 0xFF, 0);
 	cp->black = graphics_make_color(0, 0, 0, 0xFF);
 	cp->white = graphics_make_color(0xFF, 0xFF, 0xFF, 0xFF);
-	cp->lime = graphics_make_color(0x32, 0xCD, 0x32, 0xFF);
 
-	cp->bg = cp->white;
+	cp->forest_green = graphics_make_color(0x1A, 0x56, 0x53, 0xFF);
+	cp->teal_green = graphics_make_color(0x10, 0x78, 0x69, 0xFF);
+	/*cp->lime = graphics_make_color(0x32, 0xCD, 0x32, 0xFF);*/
+	cp->lime_green = graphics_make_color(0x5C, 0xD8, 0x5A, 0xFF);
+	cp->dark_forest_green = graphics_make_color(0x08, 0x31, 0x3A, 0xFF);
+
+	cp->bg = cp->dark_forest_green;
+	cp->border = cp->teal_green;
 	cp->bg_text = cp->trans;
-	cp->fg_text = cp->lime;
+	cp->fg_text = cp->white;
 
 	return cp;
 }
--- color_palette.h
+++ color_palette.h
@@ -8,9 +8,14 @@ struct color_palette
 	uint32_t trans;
 	uint32_t black;
 	uint32_t white;
-	uint32_t lime;
+
+	uint32_t forest_green;
+	uint32_t teal_green;
+	uint32_t lime_green;
+	uint32_t dark_forest_green;
 
 	uint32_t bg;
+	uint32_t border;
 	uint32_t bg_text;
 	uint32_t fg_text;
 };
