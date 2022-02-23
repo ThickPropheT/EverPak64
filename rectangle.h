083f010cc42978b24ec6e97a4671cc44ca313f02
ThickPropheT <ThickPropheT@github.com>
2022-02-22 19:00:14 -0600

    Updates parameter names for rectangle constructor and adds scaling function.

--- rectangle.h
+++ rectangle.h
@@ -13,7 +13,12 @@ struct rectangle
 	u16 h;
 };
 
-static inline struct rectangle rect_new(u16 x, u16 y, u16 w, u16 h)
+static inline struct rectangle rect_new(u16 l, u16 t, u16 w, u16 h)
 {
-	return (struct rectangle) { x, y, (x + w) - 1, (y + h) - 1, w, h };
+	return (struct rectangle) { l, t, (l + w) - 1, (t + h) - 1, w, h };
+}
+
+static inline struct rectangle rect_scale(struct rectangle r, u16 x, u16 y)
+{
+	return (struct rectangle) { r.l - x, r.t - y, r.r + x, r.b + y, (r.r - r.l) + 1, (r.b - r.t) + 1 };
 }
